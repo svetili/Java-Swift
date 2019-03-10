921f4519f4bc25adee285289764c2a8be34822f8
svetili <sveti_li@abv.bg>
2019-03-10 16:01:43 +0200

    test commit

--- Task0a_ReadInt/src/ReadInt.java
+++ Task0a_ReadInt/src/ReadInt.java
@@ -5,6 +5,7 @@ public class ReadInt {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Type number: ");
+
 		int n = sc.nextInt();
 		System.out.println("number + 1: " + (n+1));	
 		sc.close();
