5d381563fdb784723ce329230720871222425b09
svetili <sveti_li@abv.bg>
2019-04-03 23:40:26 +0300

    Task1

---
+++ Task1_UniqueNumbers/src/number/UniqueNumbers.java
@@ -0,0 +1,34 @@
+package number;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Scanner;
+
+public class UniqueNumbers {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// read number of iterations
+		System.out.println("Enter number of iterations: ");
+		int n = sc.nextInt();
+		sc.nextLine();
+		
+		// read n numbers
+		System.out.println("Enter n numbers: ");
+		Set<Integer> intUniqueList = new HashSet<Integer>();
+		for(int i = 0; i < n; i++) {
+			intUniqueList.add(sc.nextInt());
+		}
+		
+		//print the unique numbers
+		for(Integer i : intUniqueList) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+		sc.close();
+	}
+
+}
